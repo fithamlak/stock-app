91436827e1d9c80c5ba902284761bc066020aabc
Fithamlak <justiceofgod01@gmail.com>
2023-03-20 22:57:59 +0300

    install axios, Add API URL of STocks And featchStocks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => (
   <nav>
     <ul>
-      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/">Stoks</NavLink></li>
       <li><NavLink to="show">Show</NavLink></li>
       <li><NavLink to="about">About</NavLink></li>
     </ul>
--- src/redux/stocks/stocksSlice.js
+++ src/redux/stocks/stocksSlice.js
@@ -1,11 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = [];
+const STOCKS_API_URL = 'https://financialmodelingprep.com/api/v3/stock-screener?industry=Software—Application&companyName=Micron Technology, Inc.marketCapMoreThan=1000&betaMoreThan=1&volumeMoreThan=10000&sector=Technology&exchange=NYSE&dividendMoreThan=0&limit=100&apikey=c6e0dd810caa318168c7d5a3bd51bfc6';
+
+const initialState = {
+  stocksArray: [],
+  ifSucceed: false,
+  ifLoading: false,
+  errors: null,
+};
+
+export const fetchStocks = createAsyncThunk('stocks/fetchStocks', async () => {
+  try {
+    const response = await axios.get(STOCKS_API_URL);
+    return response.data;
+  } catch (err) {
+    return err.message;
+  }
+});
 
 const stocksSlice = createSlice({
   name: 'stocks',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchStocks.pending, (state) => ({
+        ...state,
+        isLoading: true,
+      }))
+      .addCase(fetchStocks.fulfilled, (state, action) => ({
+        ...state,
+        isLoading: false,
+        ifSucceed: true,
+        stocksArray: action.payload,
+      }))
+      .addCase(fetchStocks.rejected, (state) => ({
+        ...state,
+        isLoading: false,
+      }));
+  },
 });
 
-export default stocksSlice;
+export default stocksSlice.reducer;
