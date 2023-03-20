8f69978329747538283d32c7800c178c0719e792
Fithamlak <justiceofgod01@gmail.com>
2023-03-20 19:40:18 +0300

    create Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => (
+  <nav>
+    <ul>
+      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="show">Show</NavLink></li>
+      <li><NavLink to="about">About</NavLink></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
