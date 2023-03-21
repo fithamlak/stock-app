26046926a9b9094fbf5d32d81c5de1d48587a58d
Fithamlak <justiceofgod01@gmail.com>
2023-03-21 22:11:49 +0300

    Add test case when display one stock from mock store

--- src/redux/stocks/stocksSlice.test.js
+++ src/redux/stocks/stocksSlice.test.js
@@ -1,6 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import StocksContainer from '../../components/StocksContainer';
 import store from '../store';
 
-it('intially stock was empty', () => {
+const mockStore = configureStore([]);
+
+it('expects to initially set stocks as an empty object', () => {
   const state = store.getState().stocks;
   const initialState = {
     stocksArray: [],
@@ -9,4 +16,32 @@ it('intially stock was empty', () => {
     errors: null,
   };
   expect(state).toEqual(initialState);
+});
+it('display one stock from mock store', () => {
+  const stocksArray = [
+    {
+      Symbol: 'SAP',
+      companyName: 'SAP SE',
+    },
+  ];
+  const state = {
+    stocks: {
+      stocksArray,
+      ifSucceed: false,
+      ifLoading: false,
+      errors: null,
+    },
+  };
+  const store = mockStore(state);
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <StocksContainer />
+      </BrowserRouter>
+    </Provider>,
+  );
+  const companyName = screen.getByText('SAP SE');
+  expect(companyName).toBeInTheDocument();
+  const listItems = screen.getAllByRole('listitem');
+  expect(listItems).toHaveLength(1);
 });
