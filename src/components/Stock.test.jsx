26e09e00078ae0e246a2b181ae2c6fda95ff6836
Fithamlak <justiceofgod01@gmail.com>
2023-03-21 22:38:05 +0300

    add tset snapshote for stock component

---
+++ src/components/Stock.test.jsx
@@ -0,0 +1,37 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import Stock from './Stock';
+import store from '../redux/store';
+
+const SampleStock = {
+  symbol: 'CRM',
+  companyName: 'Salesforce, Inc.',
+  marketCap: 188680000000,
+  sector: 'Technology',
+  beta: 1.2094,
+  price: 188.68,
+  Exchange: 'NYSE',
+  volume: 4379287,
+  lastAnnualDividend: 0,
+};
+
+describe('test suite for Stock component', () => {
+  it('expects to render a snapshot of the Stock component', () => {
+    const stockComponent = render(
+      <Provider store={store}>
+        <Stock
+          symbol={SampleStock.symbol}
+          companyName={SampleStock.companyName}
+          sector={SampleStock.sector}
+          marketCap={SampleStock.marketCap}
+          beta={SampleStock.beta}
+          price={SampleStock.price}
+          Exchange={SampleStock.Exchange}
+          volume={SampleStock.volume}
+          lastAnnualDividend={SampleStock.lastAnnualDividend}
+        />
+      </Provider>,
+    );
+    expect(stockComponent).toMatchSnapshot();
+  });
+});
