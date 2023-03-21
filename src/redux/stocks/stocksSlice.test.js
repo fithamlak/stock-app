6285030c4295456def5d4aa8e23c81b7ef961af2
Fithamlak <justiceofgod01@gmail.com>
2023-03-21 22:14:41 +0300

    Finish test by wrapping tests with in description

--- src/redux/stocks/stocksSlice.test.js
+++ src/redux/stocks/stocksSlice.test.js
@@ -7,41 +7,43 @@ import store from '../store';
 
 const mockStore = configureStore([]);
 
-it('expects to initially set stocks as an empty object', () => {
-  const state = store.getState().stocks;
-  const initialState = {
-    stocksArray: [],
-    ifSucceed: false,
-    ifLoading: false,
-    errors: null,
-  };
-  expect(state).toEqual(initialState);
-});
-it('display one stock from mock store', () => {
-  const stocksArray = [
-    {
-      Symbol: 'SAP',
-      companyName: 'SAP SE',
-    },
-  ];
-  const state = {
-    stocks: {
-      stocksArray,
+describe('Tests for redux state of Stocks', () => {
+  it('expects to initially set stocks as an empty object', () => {
+    const state = store.getState().stocks;
+    const initialState = {
+      stocksArray: [],
       ifSucceed: false,
       ifLoading: false,
       errors: null,
-    },
-  };
-  const store = mockStore(state);
-  render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <StocksContainer />
-      </BrowserRouter>
-    </Provider>,
-  );
-  const companyName = screen.getByText('SAP SE');
-  expect(companyName).toBeInTheDocument();
-  const listItems = screen.getAllByRole('listitem');
-  expect(listItems).toHaveLength(1);
+    };
+    expect(state).toEqual(initialState);
+  });
+  it('expects to display one stock from mock store', () => {
+    const stocksArray = [
+      {
+        Symbol: 'SAP',
+        companyName: 'SAP SE',
+      },
+    ];
+    const state = {
+      stocks: {
+        stocksArray,
+        ifSucceed: false,
+        ifLoading: false,
+        errors: null,
+      },
+    };
+    const store = mockStore(state);
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <StocksContainer />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const companyName = screen.getByText('SAP SE');
+    expect(companyName).toBeInTheDocument();
+    const listItems = screen.getAllByRole('listitem');
+    expect(listItems).toHaveLength(1);
+  });
 });
