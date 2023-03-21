d79c8813888616ec3fd9b45f6067b9ea524fd1a9
Fithamlak <justiceofgod01@gmail.com>
2023-03-21 21:14:37 +0300

    display show component as child of each Stock or child of Stock component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import Stocks from './routes/Stocks';
 import Show from './routes/Show';
 import About from './routes/About';
 import NoMatch from './routes/NoMatch';
+import Stock from './routes/Stock';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Stocks />} />
-        <Route path="show" element={<Show />} />
+        <Route path="stocks" element={<Stocks />} />
+        <Route path="stock" element={<Stock />}>
+          <Route path=":slug" element={<Show />} />
+        </Route>
         <Route path="about" element={<About />} />
         <Route path="*" element={<NoMatch />} />
       </Route>
