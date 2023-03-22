28c32cd59f47cfea68d0fe76308b218f85cad544
Fithamlak <justiceofgod01@gmail.com>
2023-03-22 08:01:22 +0300

    Add styles and design for StockContainer component

--- src/components/StocksContainer.jsx
+++ src/components/StocksContainer.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { fetchStocks } from '../redux/stocks/stocksSlice';
+import '../styles/stocksContainer.scss';
 
 const StocksContainer = () => {
   const dispatch = useDispatch();
