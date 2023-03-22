f5c15f3e662cbc948c4b6cf77c9664ac1f148bd0
Fithamlak <justiceofgod01@gmail.com>
2023-03-22 09:20:25 +0300

    update Navbar to render Back button based on useParams state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
-import { NavLink } from 'react-router-dom';
-import { BsPatchQuestionFill, BsFillHouseDoorFill } from 'react-icons/bs';
+import { NavLink, useParams } from 'react-router-dom';
+import { BsPatchQuestionFill, BsFillHouseDoorFill, BsChevronLeft } from 'react-icons/bs';
 import '../styles/navbar.scss';
 
 const Navbar = () => (
   <nav>
-    <ul>
-      <li><NavLink to="/"><BsFillHouseDoorFill /></NavLink></li>
-      <li><NavLink to="/">Market Cap</NavLink></li>
-      <li><NavLink to="/"><BsPatchQuestionFill /></NavLink></li>
-    </ul>
+    {
+      Object.keys(useParams()).length === 0
+        ? (
+          <ul>
+            <li><NavLink to="/"><BsFillHouseDoorFill /></NavLink></li>
+            <li><NavLink to="/">Market Cap</NavLink></li>
+            <li><NavLink to="/"><BsPatchQuestionFill /></NavLink></li>
+          </ul>
+        )
+        : (
+          <ul>
+            <li><NavLink to="/"><BsChevronLeft /></NavLink></li>
+            <li><NavLink to="/">Campany</NavLink></li>
+            <li><NavLink to="/"><BsPatchQuestionFill /></NavLink></li>
+          </ul>
+        )
+    }
   </nav>
 );
 
