bc68b24a3e10e29aef659262107398336549376b
Fithamlak <justiceofgod01@gmail.com>
2023-03-23 08:50:38 +0300

    add handleSerch function fo search functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,22 @@
 import { NavLink, useParams } from 'react-router-dom';
 import { BsPatchQuestionFill, BsFillHouseDoorFill, BsChevronLeft } from 'react-icons/bs';
+import { AiOutlineSearch } from 'react-icons/ai';
+
 import '../styles/navbar.scss';
 
-const Navbar = () => (
-  <nav>
-    {
+const Navbar = () => {
+  const handleSearch = () => {
+    const searchFormContainer = document.getElementById('searchFormContainer');
+    searchFormContainer.classList.replace('hide', 'show');
+  };
+    <nav>
+      {
       Object.keys(useParams()).length === 0
         ? (
           <ul>
             <li><NavLink to="/"><BsFillHouseDoorFill /></NavLink></li>
             <li><NavLink to="/">Market Cap</NavLink></li>
-            <li><NavLink to="/"><BsPatchQuestionFill /></NavLink></li>
+            <li><NavLink to="/"><AiOutlineSearch onClick={handleSearch} /></NavLink></li>
           </ul>
         )
         : (
@@ -20,8 +26,8 @@ const Navbar = () => (
             <li><NavLink to="/"><BsPatchQuestionFill /></NavLink></li>
           </ul>
         )
-    }
-  </nav>
-);
+      }
+    </nav>;
+};
 
 export default Navbar;
